3bc86009cb77d684e5443720d54b779a1f0da3c7
Sivashree-117 <sivashree.pl2023@vitstudent.ac.in>
2026-01-08 17:56:37 +0530

    ow

--- one.py
+++ one.py
@@ -1,1 +1,10 @@
+# Remaining units → ₹5/unit
+    if units > 200:
+        remaining_units = units - 200
+        remaining_charge = remaining_units * 5
+    else:
+        remaining_units = 0
+
+    # Total bill
+    total = first_charge + second_charge + remaining_charge
 
